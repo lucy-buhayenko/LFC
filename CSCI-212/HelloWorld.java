265754b83a4ccf7c7d828ff290bc5780290d4556
Lucy Buhayenko <buhayenkolucy@gmail.com>
2025-12-05 18:04:54 -0600

    first java program

---
+++ CSCI-212/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Name: HelloWorld
+ * Author: Lucy Buhayenko
+ * data: 1.17.2025
+ * version: v 1.0
+ * description: write the first java program
+ */
+public class HelloWorld {
+//					main method
+	public static void main(String[] args) {
+		// output the hello world message to the console
+		System.out.println("Hello, World!");
+		System.out.println("This is a great day!");
+
+	}
+
+}
