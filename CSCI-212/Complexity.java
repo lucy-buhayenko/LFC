459e17339303d932ba7c876873b8475b38ce3337
Lucy Buhayenko <buhayenkolucy@gmail.com>
2025-12-05 18:02:18 -0600

    set and get complexity

---
+++ CSCI-212/Complexity.java
@@ -0,0 +1,13 @@
+/*
+ * name: Complexity.java
+ * author: Lucy Buhayenko
+ * date: 4.11.25
+ * version: 1
+ * description: create interface and question class
+ */
+public interface Complexity {
+	public void setComplexity(int valueComplex); //you don't need any {curly brackets} , just semi-column;
+	public int getComplexity(); 
+	//i can use this interface on the class called questions
+	
+}
